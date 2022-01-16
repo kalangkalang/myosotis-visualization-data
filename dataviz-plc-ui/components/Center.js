36d9be12ba3d59ddf1ddbb89f202d65ad0807b86
Kalang <meja.mangga@gmail.com>
2022-01-16 20:06:03 +0700

    [UI] Set data every one second

--- dataviz-plc-ui/components/Center.js
+++ dataviz-plc-ui/components/Center.js
@@ -1,13 +1,50 @@
-import { Line, Scatter } from "react-chartjs-2"
-import { BarChart, LineChart } from "./Chart"
+import { Line, Scatter } from "react-chartjs-2";
+import { BarChart, LineChart } from "./Chart";
+import { useState, useEffect } from "react";
 
 
 function Center() {
+
+    const [dataset1, setDataset1] = useState([]);
+    const [dataset2, setDataset2] = useState([]);
+    const [dataset3, setDataset3] = useState([]);
+    const [dataset4, setDataset4] = useState([]);
+
+    const updateDataset = () => {
+
+        if (dataset1.length >= 5) {
+            setDataset1([]);
+            setDataset2([]);
+            setDataset3([]);
+            setDataset4([]);
+
+            console.log("Clearing dataset array ",dataset1)
+        } else {
+            setDataset1([...dataset1, Math.random()]);
+            setDataset2([...dataset2, Math.random()]);
+            setDataset3([...dataset3, Math.random()]);
+            setDataset4([...dataset4, Math.random()]);
+        }
+
+        console.log(dataset1);
+        console.log(dataset2);
+        console.log(dataset3);
+        console.log(dataset4);
+    }
+
+    useEffect(() => {
+    
+        let periodicallyUpdateDataset = setInterval(updateDataset, 1000);
+        return () => {
+            clearInterval(periodicallyUpdateDataset);
+        }
+    }, [dataset1, dataset2, dataset3, dataset4]);
+
     return (
         <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
 
             <header className="flex my-5 justify-center">
-                <h1 className="font-bold font-mono text-white">Monitor System Haris</h1>
+                <h1 className="font-semibold font-sans underline decoration-dotted text-2xl text-white">Monitor System Haris</h1>
             </header>
 
             <div className="space-y-4">
@@ -23,8 +60,8 @@ function Center() {
 
             <div className="sticky bottom-0">
                 <div className="flex justify-start bg-slate-900 p-5 space-x-2">
-                    <button className="border-2 rounded-full py-1 px-2 text-white">Button 1</button>
-                    <button className="border-2 rounded-full py-1 px-2 text-white">Button 2</button>
+                    <button className="border-2 rounded-full py-1 px-2 text-white hover:underline">Button 1</button>
+                    <button className="border-2 rounded-full py-1 px-2 text-white hover:underline">Button 2</button>
                 </div>
             </div> 
         </div>
--- dataviz-plc-ui/components/Sidebar.js
+++ dataviz-plc-ui/components/Sidebar.js
@@ -4,25 +4,25 @@ import { ChipIcon, HomeIcon, SupportIcon, TrendingUpIcon } from "@heroicons/reac
 function Sidebar() {
 
     return (
-        <div className="text-gray-300 overflow-y-scroll scrollbar-hide border-r p-5 border-gray-900 h-screen text-xs lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
-            <div className="space-y-4">
+        <div className="text-gray-300 overflow-y-scroll scrollbar-hide border-r p-5 pt-8 border-gray-900 h-screen text-xs lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+            <div className="space-y-3">
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <HomeIcon className="h-5 w-5" />
+                    <HomeIcon className="h-4 w-4" />
                     <p>Home</p>
                 </button>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <TrendingUpIcon className="h-5 w-5"/>
+                    <TrendingUpIcon className="h-4 w-4"/>
                     <p>Monitor-System</p>
                 </button>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <ChipIcon className="h-5 w-5" />
+                    <ChipIcon className="h-4 w-4" />
                     <p>Status-System</p>
                 </button>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <SupportIcon className="h-5 w-5" />
+                    <SupportIcon className="h-4 w-4" />
                     <p>Support</p>
                 </button>
             </div>
